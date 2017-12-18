9a6893cdeaa576d852aac62b05c94125ae3755ab
Jason Torres <jmtorres@elder.(none)>
2017-12-18 06:46:25 +0000

    added R files

---
+++ fgwas/diagram_hrc/cross_tissue/05.0_functional_credible_sets.R
@@ -0,0 +1,115 @@
+
+#Constructing functional (fgwas) credible sets from the fgwas model with the best cross validated likelihood
+
+# Setup
+args = commandArgs(trailingOnly=TRUE)
+loc.id <- args[1]
+
+"%&%" <- function(a,b) paste0(a,b)
+library("data.table")
+library("dplyr")
+library("ggplot2")
+library(GenomicRanges)
+
+serv.dir <- "/well/got2d/jason/"
+
+work.dir <- serv.dir %&% "projects/t2d-integration/fgwas/diagram_hrc/cross_tissue/"
+fgwas.output.dir <- work.dir %&% "conditional/fgwas_output_files/" %&% loc.id %&% "/"
+
+
+cred.set.dir <- serv.dir %&% "projects/t2d-integration/fgwas/diagram_hrc/cross_tissue/credible_sets/"
+
+print("Making credible set file for locus: " %&% loc.id)
+
+
+get_cred <- function(dframe,cname,prob=0.99){ # 99% functional credible sets
+  index <- match(cname,names(dframe))
+  vec <- sort(dframe[,index],decreasing=TRUE)
+  count=0
+  sum=0
+  for (v in vec){
+    count <- count + 1
+    sum <- sum + v
+    if (sum >= prob){
+      break
+    }
+  }
+  return(count)
+}
+
+# Determine functional credible sets (95% and 99% Credible Sets)
+
+get_credsets <- function(probthresh,segsnps.df){
+  seg.vec <- sort(unique(segsnps.df$SEGNUMBER))
+  out.df <- c()
+  pb <- txtProgressBar(min=0,max=length(seg.vec),style = 3)
+  for (i  in 1:length(seg.vec)){
+    setTxtProgressBar(pb,i)
+    seg <- seg.vec[i]
+    temp.df <- filter(segsnps.df,SEGNUMBER==seg) %>% arrange(desc(PPA))
+    cumppa <- sum(temp.df$PPA)
+    temp.df <- temp.df[1:get_cred(temp.df,"PPA",prob=probthresh*cumppa),]
+    temp.df <- dplyr::select(temp.df,-pi,-chunk,-pseudologPO,-pseudoPPA,-V)
+    names(temp.df)[c(2,3,4)] <- c("SNPID","CHR","POS")
+    temp.df$PPA <- temp.df$PPA/cumppa # rescale ppa to reflect proportion of cummulative sum in block
+    out.df <- rbind(out.df,temp.df)
+  }
+  return(out.df)
+}
+
+# Get nearest genes
+
+library(GenomicFeatures)
+library(org.Hs.eg.db)
+library(annotate)
+
+annot_refGene <- function(cred.df,segsnps.df){
+  seg.vec <- sort(unique(segsnps.df$SEGNUMBER))
+  snp.gr <- GRanges(cred.df$CHR,IRanges(cred.df$POS, cred.df$POS))
+
+  hg19.refseq.db <- makeTxDbFromUCSC(genome="hg19", table="refGene")
+  refseq.genes<- genes(hg19.refseq.db)
+  all.geneids <- elementMetadata(refseq.genes)$gene_id
+  all.genesymbols <- getSYMBOL(all.geneids, data='org.Hs.eg')
+  ref.df <- data.frame(gene.id=all.geneids,symbol=all.genesymbols,
+                       stringsAsFactors = FALSE)
+  elementMetadata(refseq.genes)$gene_id <- all.genesymbols
+  df <- as.data.frame(refseq.genes)
+  sub.gr <- GRanges(seqnames=df$seqnames,
+                    IRanges(start=df$start,end=df$end),
+                    strand=rep("*",dim(df)[1]),
+                    name=df$gene_id)
+  nearestGenes <- nearest(snp.gr,sub.gr)
+  res <- df$gene_id[nearestGenes]
+  dist <- distance(snp.gr, refseq.genes[nearestGenes])
+  symbol <- res
+  cred.df <- cbind(symbol,cred.df)
+
+
+  # Sync symbol names for each SEGNUMBER by MOST COMMON gene
+  sync.df <- c()
+  pb <- txtProgressBar(min=0,max=length(seg.vec),style=3)
+  for (i in 1:length(seg.vec)){
+    setTxtProgressBar(pb,i)
+    seg <- seg.vec[i]
+    temp.df <- filter(cred.df,SEGNUMBER==seg)
+    top <- names(sort(table(temp.df$symbol),decreasing=TRUE))[1]
+    temp.df$symbol <- rep(top,dim(temp.df)[1])
+    sync.df <- rbind(sync.df,temp.df)
+  }
+  cred.df <- sync.df
+  return(cred.df)
+}
+
+# Run
+
+
+segsnps.df <- fread("cat " %&% fgwas.output.dir  %&% "loci_block_snps.bfs.txt.gz" %&% " | zmore")
+cred95.df <- get_credsets(0.95,segsnps.df)
+cred99.df <- get_credsets(0.99,segsnps.df)
+cred95.df <- annot_refGene(cred95.df,segsnps.df)
+cred99.df <- annot_refGene(cred99.df,segsnps.df)
+write.table(x=cred95.df,file=cred.set.dir%&%"fgwas_credsets_95-" %&% loc.id %&% ".txt",sep="\t",
+            quote=FALSE,row.names=F)
+write.table(x=cred99.df,file=cred.set.dir%&%"fgwas_credsets_99-" %&% loc.id %&% ".txt",sep="\t",
+            quote=FALSE,row.names=F)
---
+++ fgwas/diagram_hrc/cross_tissue/05.0_get-cond-block.R
@@ -0,0 +1,61 @@
+
+# Setup
+
+args = commandArgs(trailingOnly=TRUE)
+loc.id <- args[1]
+
+"%&%" <- function(a,b) paste0(a,b)
+
+library("data.table")
+library("dplyr")
+library("ggplot2")
+library("GenomicRanges")
+
+serv.dir <- "/well/got2d/jason/"
+
+eur.dir <- serv.dir %&% "projects/t2d-integration/fgwas/diagram_hrc/ukbb-diamante-euro-manuscript/"
+
+work.dir <- serv.dir %&% "projects/t2d-integration/fgwas/diagram_hrc/cross_tissue/"
+
+fgwas.output.dir <- work.dir %&% "conditional/fgwas_output_files/" %&% loc.id %&% "/"
+pre <- fgwas.output.dir %&% "fgwas_run_loci-partition"
+
+region.file <- eur.dir %&% "region_files/" %&% "t2d-loci-regions-" %&% loc.id %&% ".txt"
+
+
+get_loci_blocks <- function(save.prefix){
+  loc.df <- fread(region.file)
+  names(loc.df) <- c("CHR","Start","End")
+  loc.gr <- GRanges(seqnames=loc.df$CHR,IRanges(loc.df$Start,loc.df$End))
+  blk.df <- fread("cat " %&% pre %&% ".segbfs.gz" %&% " | zmore")
+  blk.gr <- GRanges(seqnames=blk.df$chr,IRanges(blk.df$st,blk.df$sp))
+  keep.gr <- blk.gr[blk.gr%over%loc.gr]
+  out.df <- c()
+  pb <- txtProgressBar(min=0,max=dim(loc.df)[1],style=3)
+  for (i in 1:length(loc.gr)){
+    setTxtProgressBar(pb,i)
+    build.gr <- blk.gr[blk.gr%over%loc.gr[i]]
+    chrom.vec <- as.character(seqnames(build.gr))
+    start.vec <- start(build.gr)
+    end.vec <- end(build.gr)
+    build.df <- filter(blk.df,chr%in%chrom.vec,st%in%start.vec,sp%in%end.vec)
+    SEGNUMBER <- rep(i,length(chrom.vec))
+    build.df <- cbind(SEGNUMBER,build.df) %>% arrange(desc(PPA))
+    if (dim(build.df)[1]>1){
+      ref.point <- (end(loc.gr[i]) + start(loc.gr[i]))/2
+      build.df <- filter(build.df,st<=ref.point,sp>=ref.point)
+    }
+    out.df <- rbind(out.df,build.df[1,])
+  }
+  sig.blk.df <- dplyr::select(out.df,one_of("SEGNUMBER","chr","st","sp","PPA","NSNP"))
+
+  names(sig.blk.df) <- c("SEGNUMBER","CHR","blk.start","blk.end","blk.PPA","blk.NSNP")
+
+
+  write.table(x=sig.blk.df,file = save.prefix %&%".txt",
+              sep="\t",quote=FALSE,row.names=FALSE)
+  return(sig.blk.df)
+}
+
+save.name <- fgwas.output.dir %&% "fgwas_blk"
+loc.blk.df <- get_loci_blocks(save.name)
